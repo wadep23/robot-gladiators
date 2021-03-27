3d4079ce99c59f7a268272d8be5d3827ceba61ec
wadep <wadepfacer@gmail.com>
2021-03-27 15:09:13 -0600

    added random math generator

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,7 +24,7 @@ var fight = function(enemyName){
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
             if (confirmSkip){
                 window.alert(playerName + " has chosen to skip the fight. Goodbye!");
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log('Player money',  playerMoney);
                 break;
             }
@@ -35,7 +35,9 @@ var fight = function(enemyName){
         // if player chooses to fight
         
         // Subtract the value of 'playerAttack from the velue of 'enemyHealth' and use that result to update the value of 'enemyHealth
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
     
         if (enemyHealth <=0){
@@ -46,7 +48,8 @@ var fight = function(enemyName){
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
         // subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value of 'playerHealth'
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
 
         console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
@@ -67,7 +70,7 @@ var startGame = function() {
         if (playerHealth > 0){
             window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
 
             if (playerHealth > 0 && i < enemyNames.length - 1){
@@ -101,7 +104,7 @@ var endGame = function(){
 };
 var shop = function(){
     var shopOptionPrompt = window.prompt('Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?');
-
+// switch cases used in place of if else statements
     switch(shopOptionPrompt){
         case "REFILL":
         case "refill":
@@ -137,6 +140,12 @@ var shop = function(){
             
             shop();
             break;
+            // switches must include a break
     }
 };
+var randomNumber = function(min, max){
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
 startGame();
