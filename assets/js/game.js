8cc0edea764895cd9195c6c431ea3babbd766962
wadep <wadepfacer@gmail.com>
2021-03-26 18:38:33 -0600

    added multiple enemy loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,22 @@
+// Game States
+// "Win" - Player robot has defeated all enemy-robots
+//  *Fight all enemy-robots
+//  *Defeat each enemy-robot
+// "Lose" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robots name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(){
+var fight = function(enemyName){
+    
+    console.log(
+        "Player robot's name is " + playerName + " !");
     window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -55,4 +64,6 @@ var fight = function(){
 }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
